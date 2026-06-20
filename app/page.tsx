3a67ba88b5a2e324137f44baf174a3964ed709f8
ImperioKDB <kingbrite123@gmail.com>
2026-06-20 18:27:16 +0100

    Insert AboutTerminal section into homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Navbar from "@/components/Navbar";
 import Hero from "@/components/Hero";
 import StackIcons from "@/components/StackIcons";
+import AboutTerminal from "@/components/AboutTerminal";
 import ProjectsGrid from "@/components/ProjectsGrid";
 import Testimonials from "@/components/Testimonials";
 import Timeline from "@/components/Timeline";
@@ -12,6 +13,7 @@ export default function Home() {
       <Navbar variant="home" />
       <Hero />
       <StackIcons />
+      <AboutTerminal />
       <ProjectsGrid />
       <Testimonials />
       <Timeline />
