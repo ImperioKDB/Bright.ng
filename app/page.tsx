d1ef3e9f60cb1d37c086dbdf7f82ac5a8cc38a33
ImperioKDB <kingbrite123@gmail.com>
2026-06-21 11:02:12 +0100

    REVERT: remove AboutTerminal from homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 import Navbar from "@/components/Navbar";
 import Hero from "@/components/Hero";
 import StackIcons from "@/components/StackIcons";
-import AboutTerminal from "@/components/AboutTerminal";
 import ProjectsGrid from "@/components/ProjectsGrid";
 import Testimonials from "@/components/Testimonials";
 import Timeline from "@/components/Timeline";
@@ -13,7 +12,6 @@ export default function Home() {
       <Navbar variant="home" />
       <Hero />
       <StackIcons />
-      <AboutTerminal />
       <ProjectsGrid />
       <Testimonials />
       <Timeline />
